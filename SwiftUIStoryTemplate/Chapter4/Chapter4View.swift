2fc7abc199201d6262d98d79843e08e273aeee7c
Gicentroy Henry III <gicentroyhenry3@gmail.com>
2023-11-17 07:47:27 -0500

    Chapter 4 changes

--- SwiftUIStoryTemplate/Chapter4/Chapter4View.swift
+++ SwiftUIStoryTemplate/Chapter4/Chapter4View.swift
@@ -1,54 +1,85 @@
 import SwiftUI
 
 struct Chapter4View: View {
+    @State private var offset: CGFloat = 0
+    let teamSchedule = "Eagles vs Vikings, Eagles vs Steelers, Eagles vs Lions, Eagles vs 49ers, Eagles vs Ravens, Eagles vs Steelers"
+
     var body: some View {
-        ScrollView{
-            ZStack{
+        ScrollView {
+            ZStack {
                 Image("football")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 400, height: 500)
-            
-           }
-            VStack{
+            }
+            VStack {
                 Text("Chapter 4")
                     .font(.title)
                     .bold()
                     .colorInvert()
                     .background(.black)
                     .padding()
-            Divider()
-                
-                    Text("After getting settled in his home Kevin reflected on the eventful day he had.The Eagles beat the Commanders and David had a good time. This day couldn’t have gone any better!” He thought to himself as he sat in his favorite seat.")
-                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                Divider()
+
+                Text("After getting settled in his home Kevin reflected on the eventful day he had.The Eagles beat the Commanders and David had a good time. This day couldn’t have gone any better!” He thought to himself as he sat in his favorite seat.")
+                    .shadow(radius: 10)
                     .padding()
-                    Text("Kevin was pleased. He hoped that maybe one day David would continue the tradition with his children in the future and thought about 3 games he coming in the next couple of weeks:")
-                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+
+                Text("Kevin was pleased. He hoped that maybe one day David would continue the tradition with his children in the future and thought about 3 games he coming in the next couple of weeks:")
+                    .shadow(radius: 10)
                     .padding()
-                
-                    Text("Team Schedule")
+
+                Text("Kevin thought Marsha has to come to the next game with us. The win today was one for the books. Kevin asked Marsha, and she replied, 'Of course, and next time we’ll bring the whole family!'")
+                    .shadow(radius: 10)
+                    .padding()
+
+                // Sports ticker without marquee effect
+                Text("Team Schedule")
                     .bold()
                     .font(.title2)
-             
-                //Want to make a sports ticker with team schedule
-                //Need animation and possibly hStack and infinity
-                    Text("Eagles vs Vikings, Eagles vs Steelers, Eagles vs Lions")
-                    .padding()
-                    .font(.title)
-                    .bold()
-                    .colorInvert()
-                    .background(.red)
-            
-    
-                    Text("Kevin thought Marsha has to come to the next game with us the win today was one for the books. Kevin asked Marsha and she replied “Of course, and next time we’ll bring the whole family!")
-                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                    .padding()
+
+                ScrollView(.horizontal, showsIndicators: false) {
+                    Text(teamSchedule)
+                        .padding()
+                        .font(.caption)
+                        .bold()
+                        .colorInvert()
+                        .background(.red)
+                        .offset(x: offset)
+                        .onAppear {
+                            startTicker()
+                        }
+                }
+
+                NavigationLink(destination: Chapter5View()) {
+                    Image(systemName: "chevron.right")
+                        .font(.system(size: 60))
+                        .foregroundStyle(.black)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .trailing)
+                        .padding()
+                }
             }
-                
         }
+    }
+
+    // Function to start the ticker animation
+    func startTicker() {
+        _ = Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true) { timer in
+            withAnimation {
+                offset -= 1
+            }
+
+            // Adjust the multiplier based on the speed of the ticker
+            if offset <= -CGFloat(teamSchedule.count) * 20 {
+                offset = 400
+            }
         }
+    }
 }
 
-#Preview {
-    Chapter4View()
+struct Chapter4View_Previews: PreviewProvider {
+    static var previews: some View {
+        Chapter4View()
+    }
 }
+
